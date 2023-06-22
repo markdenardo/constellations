fb7356ca6d3f6cb8ccff96d8db754393b39027df
MDN <mark.denardo@gmail.com>
2023-06-21 23:46:23 -0400

    Update red-shift.js

--- red-shift.js
+++ red-shift.js
@@ -1,4 +1,10 @@
 //mdn
-//red shift
-const f=(()=>a.fft[0])
-noise(1,1,1).scale(0.5,0.5).color(1,f,f).repeat(()=>time,0.1).modulateScale(osc(1,1,1),f).add(o0,0.5).out()
+//time dilation
+const f = (() => a.fft[0])
+noise(1, 1, 1)
+	.scale(0.5, 0.5)
+	.color(1, f, f)
+	.repeat(() => time, 0.1)
+	.modulateScale(osc(1, 1, 1), f)
+	.add(o0, 0.5)
+	.out()
